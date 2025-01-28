ee6a6fb746341d9d2d508c5d62b0e27df9c92f51
Artalus <artalus-mail@yandex.ru>
2025-01-29 00:17:19 +0400

    add dev command

--- do.ps1
+++ do.ps1
@@ -7,6 +7,7 @@ $ErrorActionPreference = "Stop"
 
 switch ($Command) {
     "junction" {}
+    "dev" {}
     "release" {}
     "vsPostBuild" {}
     "vsPreBuild" {}
@@ -41,11 +42,11 @@ function Do_junction {
     if (Test-Path $JUNCTION_TARGET) {
         if ((Get-Item -Path $JUNCTION_TARGET -Force).LinkType -eq "Junction") {
             Write-Host "-- Junction ${JUNCTION_TARGET} already exists; do nothing"
-            return 0
+            return
         }
         else {
             Write-Error "-- Path ${JUNCTION_TARGET} exists but is not a junction"
-            return 1
+            return
         }
     }
     Write-Host "-- Create junction $JUNCTION_TARGET"
@@ -53,13 +54,25 @@ function Do_junction {
     return 0
 }
 
+function Do_dev {
+    if ("${env:RIMWORLD}" -eq "") {
+        Write-Warning "Cannot use 'dev': specify path to RimWorld, like `$env:RIMWORLD='E:\SteamLibrary\steamapps\common\RimWorld\RimWorldWin64.exe'"
+        return
+    }
+    _removeInstalledMod
+    Do_junction
+    Write-Host "-- Build 'Release' config"
+    & dotnet build -c Release
+    & ${env:RIMWORLD}
+}
+
 # build & pack the mod for publishing from inside the game main menu
 function Do_release {
     Write-Host "-- Remove dev junction"
     _unjunction
 
-    Write-Host "-- Build 'Release' config"
     _gitEnsureEverythingCommitted
+    Write-Host "-- Build 'Release' config"
     & dotnet build -c Release
 
     if (Test-Path "./LeTimer") {
@@ -69,10 +82,7 @@ function Do_release {
     Copy-Item -Recurse "./mod" "./LeTimer"
     Compress-Archive "./LeTimer" -DestinationPath "LeTimer.zip" -Force
 
-    if (Test-Path "${DIR_RELEASED_MOD}") {
-        Write-Host "-- Remove old installed mod folder"
-        Remove-Item -Recurse "${DIR_RELEASED_MOD}"
-    }
+    _removeInstalledMod
 
     Move-Item "./LeTimer" "${DIR_RELEASED_MOD}"
     Write-Host "-- SUCCESS! Start RimWorld, enable Devmode, open Mod, publish to Steam"
@@ -111,7 +121,7 @@ function _gitEnsureEverythingCommitted {
     Write-Host "-- Ensure no uncommitted changes in ${PWD}"
     if (!(Get-Command "git" -ErrorAction SilentlyContinue)) {
         Write-Warning "-- 'git' not available; do nothing and pray for the best"
-        return 0
+        return
     }
 
     # courtesy of https://stackoverflow.com/a/3879077
@@ -121,7 +131,7 @@ function _gitEnsureEverythingCommitted {
         Write-Host "-- Changes detected:"
         git diff-files --name-status -r --ignore-submodules --
         Write-Error "-- Unstaged changes in the working tree (see above)"
-        return 1
+        return
     }
 
     & git diff-index --cached --quiet HEAD --ignore-submodules -- .
@@ -129,11 +139,18 @@ function _gitEnsureEverythingCommitted {
         Write-Host "-- Changes detected:"
         & git diff-index --cached --name-status -r --ignore-submodules HEAD --
         Write-Error "-- Uncommitted changes in index (see above)"
-        return 1
+        return
     }
     Write-Host "-- All good; now do git clean just to be sure :E"
     Write-Host "   (this is just interactive preview, even if you hit C nothing gets deleted)"
     & git clean --dry-run -x -i ./mod ./src
+}
+
+function _removeInstalledMod {
+    if (Test-Path "${DIR_RELEASED_MOD}") {
+        Write-Host "-- Remove old installed mod folder"
+        Remove-Item -Recurse "${DIR_RELEASED_MOD}"
+    }
 }
 
 & "Do_${Command}"
